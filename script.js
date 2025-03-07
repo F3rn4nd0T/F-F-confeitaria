ee3635f3578d9d81bcda4bf1a72378a849e235bb
F3rn4nd0T <fernandotito69@gmail.com>
2025-03-07 11:04:43 -0300

    nav lateral

---
+++ script.js
@@ -0,0 +1,11 @@
+function fecharNAv(){
+    document.getElementyById("menuoculto").style.widht="250px"
+    document.getElementyById("barralateral").style.marginleft="250px"
+
+}
+
+function abrirNav() {
+    document.getElementyById("menuoculto").style.widht="0"
+    document.getElementyById("barralateral").style.marginleft="0"
+    
+}
